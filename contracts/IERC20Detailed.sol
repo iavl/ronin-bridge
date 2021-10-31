733b16af1c4bd99d15a8e7708184d2010ccf2aab
albert <albert@rss3.io>
2021-10-31 10:29:26 +0800

    add

---
+++ contracts/IERC20Detailed.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.2;
+
+
+interface IERC20Detailed {
+    function name() external view returns (string memory _name);
+    function symbol() external view returns (string memory _symbol);
+    function decimals() external view returns (uint8 _decimals);
+}
